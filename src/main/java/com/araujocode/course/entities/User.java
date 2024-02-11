8109909ee515267a0609ab3647ac7b9fabc78392
Bruno Araujo <bruno.cadoma@gmail.com>
2024-02-11 01:54:08 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/araujocode/course/entities/User.java
+++ src/main/java/com/araujocode/course/entities/User.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
@@ -19,6 +22,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -72,6 +78,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -83,4 +93,6 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hash(getId());
     }
+
+
 }
