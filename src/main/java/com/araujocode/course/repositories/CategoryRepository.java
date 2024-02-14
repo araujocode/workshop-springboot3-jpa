6bc2c933a04afb87c148ed434f8059d664d7760c
Bruno Araujo <bruno.cadoma@gmail.com>
2024-02-14 00:39:08 -0300

    Category

---
+++ src/main/java/com/araujocode/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.araujocode.course.repositories;
+
+import com.araujocode.course.entities.Category;
+import com.araujocode.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/main/java/com/araujocode/course/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.araujocode.course.services;
+
+import com.araujocode.course.entities.Category;
+import com.araujocode.course.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
